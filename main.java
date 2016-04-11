710285324de508f82f58db4f799aaabb4c620f6e
MasterTrollVG78 <MasterTrollVG78@users.noreply.github.com>
2016-04-11 18:42:01 +0200

    Delete main.java

--- main.java
+++
@@ -1,9 +0,0 @@
-package com.esiea.glpoo.pokemon;
-
-public class main {
-	public static void main(String[] args) {
-		System.out.println("Coucou!");
-		new Frame.getInstance();
-		new Pokemon();
-	}
-}
