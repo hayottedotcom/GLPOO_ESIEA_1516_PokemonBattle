940fa0d4c582ac233eb48c2edd62e86a83bbfc91
MasterTrollVG78 <MasterTrollVG78@users.noreply.github.com>
2016-04-11 18:41:18 +0200

    Added files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+package com.esiea.glpoo.pokemon;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("Coucou!");
+		new Frame.getInstance();
+		new Pokemon();
+	}
+}
