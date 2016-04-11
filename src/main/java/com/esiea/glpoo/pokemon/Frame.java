00a90116ac8b61a66a0f9f09793cde98ffe4e24e
MasterTrollVG78 <MasterTrollVG78@users.noreply.github.com>
2016-04-11 18:42:32 +0200

    Added files via upload

--- src/main/java/com/esiea/glpoo/pokemon/Frame.java
+++ src/main/java/com/esiea/glpoo/pokemon/Frame.java
@@ -13,20 +13,32 @@ import javax.swing.JLabel;
 
 public class Frame {
 	
+	private static Frame instance = null;
+	
+
 	ImagePoke fond= new ImagePoke();
 	JFrame fenetre;
-	Frame(){
-	Panel panel=new Panel(fond.Fond2);
-    fenetre = new JFrame("Pokemon");
-    //fenetre.setContentPane(new JLabel(fond.Fond));
-    fenetre.setSize(640,480);
-    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    fenetre.setContentPane(panel);
-	//fenetre.pack();
-    fenetre.setFocusable(false);
-    fenetre.setResizable(false);
-	fenetre.setVisible(true);
+	
+	private Frame(){
+		Panel panel=new Panel(fond.Fond2);
+		fenetre = new JFrame("Pokemon");
+		//fenetre.setContentPane(new JLabel(fond.Fond));
+		fenetre.setSize(640,480);
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetre.setContentPane(panel);
+		//fenetre.pack();
+		fenetre.setFocusable(false);
+		fenetre.setResizable(false);
+		fenetre.setVisible(true);
 	}
+
+	public static Frame getInstance(){
+		if(instance == null){
+			instance = new Frame();
+		}
+		return instance;
+	}
+
 	public Dimension getFenetreSize() {
 		return fenetre.getSize();
 	}
