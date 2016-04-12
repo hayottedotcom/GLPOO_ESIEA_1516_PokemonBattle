ec40fc2e9fa650df8eabedcd0efe391af46b3cfc
MasterTrollVG78 <MasterTrollVG78@users.noreply.github.com>
2016-04-12 20:09:38 +0200

    Update Frame.java

--- src/main/java/com/esiea/glpoo/pokemon/Frame.java
+++ src/main/java/com/esiea/glpoo/pokemon/Frame.java
@@ -13,10 +13,10 @@ import javax.swing.JLabel;
 
 public class Frame {
 	Panel panel;
-	ImagePoke fond= new ImagePoke();
+	ImagePoke fond= new ImagePoke.getInstance();
 	JFrame fenetre;
 	Frame(){
-	panel=new Panel(fond.Fond2);
+	panel=new Panel(fond.Fond2); // fond ou Fond ???
     fenetre = new JFrame("Pokemon");
     //fenetre.setContentPane(new JLabel(fond.Fond));
     fenetre.setSize(640,480);
